548f2396dd2353514541e04439aa795e6a79768e
KOUASSI CHARLES OTH-NIEL <kouassicharlesothniel@gmail.com>
2021-01-22 08:13:15 +0000

    add

--- index.js
+++ index.js
@@ -66,43 +66,44 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(n => n.id))
+  : 0
+  console.log('maxId', maxId)
+  return maxId + 1
+}
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log('body', body)
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name and number is void' 
+    })
+  }
+  if(persons.map(val => val.name).includes(body.name) !=""){
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+  
+  persons = persons.concat(person)
+  
+  response.json(person)
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-
-const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-      console.log('maxId', maxId)
-    return maxId + 1
-  }
-
-  
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-    console.log('body', body)
-  
-    if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'name and number is void' 
-      })
-    }
-    if(persons.map(val => val.name).includes(body.name) !=""){
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-          })
-       }
-
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId(),
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
-  })
